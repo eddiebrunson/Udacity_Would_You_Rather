9c87378365cc69617ff2c3cdef7c0081810314d2
Eddie Brunson <eddiebrunson@gmail.com>
2018-08-26 03:28:29 -0400

    Add Reducers files

---
+++ src/reducers/users.js
@@ -0,0 +1,16 @@
+import { RECEIVE_USER } from '../actions/users'
+
+export default function users (state = {}, action){
+  switch(action.type) {
+    case RECEIVE_USERS :
+      return {
+      	state,
+      	action.users
+      }
+    default :
+      return state
+
+  }
+
+}
+
