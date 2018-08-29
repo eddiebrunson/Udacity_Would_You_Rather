7d97bd99c94474a15d63138fc88a8c6ab08102d6
Edward Brunson <eddiebrunson@gmail.com>
2018-08-29 08:55:12 -0400

    Update redcuers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
+import { loadingBarReducer } from 'react-redux-loading' 
 
+/* combine all the reducers into one root reducer */
+/* since the createStore function only accepts a single reducer */
 export default combineReducers({
   authedUser,
   users,
-  questions
+  questions,
+  loadingBar: loadingBarReducer,
 })
 
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,16 +1,38 @@
-import { RECEIVE_USER } from '../actions/users'
+import { RECEIVE_USERS, UPDATE_USER_ANSWER } from '../actions/users'
+import { ADD_QUESTION } from '../actions/questions'
 
-export default function users (state = {}, action){
+export default function users (state = [], action) {
+
+  const user = { ...state[action.authedUser] }
+  const answer = action.answer
+  const question = action.question
+
   switch(action.type) {
     case RECEIVE_USERS :
       return {
-      	state,
-      	action.users
+        ...state,
+        ...action.users,
+      }
+    case UPDATE_USER_ANSWER :
+      return {
+        ...state,
+        [action.authedUser] : {
+          ...user,
+          answers: {
+            ...user.answers,
+            [action.id] : answer
+          }
+        }
+      }
+    case ADD_QUESTION :
+      return {
+        ...state,
+        [question.author] : {
+          ...state[question.author],
+          questions: state[question.author].questions.concat([question.id]),
+        }
       }
     default :
       return state
-
   }
-
 }
-
