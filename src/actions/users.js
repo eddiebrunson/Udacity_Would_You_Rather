d61759cfaeac6496fed949e855e526bcb6df9fdc
Edward Brunson <eddiebrunson@gmail.com>
2018-08-29 08:53:44 -0400

    Add action/users

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,33 @@
+import {saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const UPDATE_USER_ANSWER = 'UPDATE_USER_ANSWER'
+export const UPDATE_USER_QUESTION = 'UPDATE_USER_QUESTION'
 
 export function receiveUsers (users) {
-	return {
-		type: RECEIVE_USERS,
-		users,
-	}
+  return {
+    type: RECEIVE_USERS,
+    users,
+  }
+}
+
+function updateUserAnswer({qid, authedUser, answer}) {
+  return {
+    type: UPDATE_USER_ANSWER,
+    id: qid,
+    authedUser,
+    answer,
+  }
+}
+
+export function handleUpdateUserAnswer(info) {
+  return dispatch => {
+    dispatch(updateUserAnswer(info))
+    return saveQuestionAnswer(info)
+      .catch((e) => {
+        console.warn("Error in handleAnswerQuestion")
+        dispatch(updateUserAnswer(info))
+        alert('There was an error with processing this question.')
+      })
+  }
 }
